427e5333c09e62e23d8f5a66b8254168e7fb0a5a
Lunático <alejandro.ramirezc@gmail.com>
2021-01-20 19:43:23 -0600

    Programas basicos

---
+++ 01_introduccion/for.py
@@ -0,0 +1,8 @@
+numero = 10
+contador = 0
+
+for i in range (1, numero+1):
+    if (numero%i)==0:
+        contador = contador + 1
+
+print (contador)
---
+++ 01_introduccion/while.py
@@ -0,0 +1,11 @@
+contador = 0
+
+while(contador < 5):
+    print ("El contador es " + str(contador))
+    contador = contador + 1
+
+    if(contador > 3):
+        break
+
+# fin de while
+print ("Fin del programa")
